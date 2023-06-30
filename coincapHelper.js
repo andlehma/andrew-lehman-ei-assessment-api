b83f998b7671c9bb2157d2574da72b444493852c
Andrew Lehman <andrew.lehman364@gmail.com>
2023-06-29 21:30:36 -0500

    api calls coincap api to get assets

---
+++ coincapHelper.js
@@ -0,0 +1,18 @@
+import got from 'got';
+import 'dotenv/config';
+
+const api_key = process.env.COINCAP_API_KEY;
+
+const gotOptions = {
+	prefixUrl: 'https://api.coincap.io/v2',
+	headers: {
+		Authorization: `Bearer ${api_key}`,
+	},
+};
+
+const getAssets = async () => {
+	const { data } = await got.get('assets', gotOptions).json();
+	return data;
+};
+
+export { getAssets };
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require("express");
-require('dotenv').config();
-const api_key = process.env.COINCAP_API_KEY
+import express from 'express';
+
+import { getAssets } from './coincapHelper.js';
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello, World!");
+app.get('/', async (req, res) => {
+	const data = await getAssets();
+	res.send(data);
 });
 
 app.listen(port, () => {
-	console.log(`Hello World app running on port ${port}`);
+	console.log(`API running on port ${port}`);
 });
